97679ce9f2bb5cf57801b18e10747bd23839ac90
Tadukoo <realtadukoo@gmail.com>
2020-12-28 11:23:47 -0500

    Corrected Some Docs
    - Because of how I had some stuff setup in IntelliJ, the docs were a bit messed up and have now been fixed

--- TadukooEngine/current/package-search-index.js
+++ TadukooEngine/current/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.github.tadukoo.engine"},{"l":"com.github.tadukoo.launcher"},{"l":"com.github.tadukoo.launcher.downloader"}]
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.github.tadukoo.engine"},{"l":"com.github.tadukoo.launcher"},{"l":"com.github.tadukoo.launcher.downloader"}];updateSearchResults();
